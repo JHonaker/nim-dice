ed304f79186bff145e434a4ea207ab8424abe130
JHonaker <honaker215@gmail.com>
2017-05-19 17:28:28 -0400

    Added keepHighest, keepLowest functions

--- dice.nim
+++ dice.nim
@@ -1,10 +1,12 @@
 import random
 import sequtils
+import math
+import algorithm
 
 type
-  Die[T] = seq[T]
+  Die = seq[int]
 
-proc d(N: int): Die[int] =
+proc d(N: int): Die =
   # Generator function for polyhedral dice with integer faces
   # Calling this returns a die with faces 1..N:
   #   d(3) = @[1, 2, 3]
@@ -12,14 +14,46 @@ proc d(N: int): Die[int] =
   #   etc.
   toSeq(1..N)
 
-proc roll[T](die: Die[T]): T =
-  # Rolls any die and chooses a face.
+# Some common polyhedral dice used in RPGs
+let
+  d4* = d(4)
+  d6* = d(6)
+  d8* = d(8)
+  d10* = d(10)
+  d12* = d(12)
+  d20* = d(20)
+  d100* = d(100)
+  dPercent* = d10.map do (x: int) -> int : (x - 1) * 10
+
+proc roll(die: Die): int =
   random(die)
 
-var d20 = d(20)
-var dStr: Die[string] = @["a", "b", "c"]
+proc roll(die: Die, N: int): seq[int] =
+  # Rolls any number of die and chooses a face.
+  assert(N > 0)
+  newSeq(result, N)
+  for i in 0..<N:
+    result[i] = die.roll
+
+proc keepHighest(rolls: seq[int], N: int): seq[int] =
+  if N < 0:
+    result = @[]
+  elif 0 <= N and N <= rolls.len:
+    result = rolls.sorted(system.cmp[int])[(rolls.len - N)..<rolls.len]
+  else:
+    result = rolls
+
+proc keepLowest(rolls: seq[int], N: int): seq[int] =
+  if N < 0:
+    result = @[]
+  elif 0 <= N and N <= rolls.len:
+    result = rolls.sorted(system.cmp[int])[0..<N]
+  else:
+    result = rolls
+
 
 if isMainModule:
-  echo d20.roll
-  echo d20.roll
-  echo dStr.roll
+  echo dPercent.roll
+  echo d20.roll(10).keepHighest(4)
+  echo d20.roll(11).keepLowest(5)
+  echo roll(d20, 3)
